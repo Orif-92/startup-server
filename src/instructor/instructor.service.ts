48afb4fb19b14c58237138b488d91a74cc0ffb0a
Orif-92 <sarniyozovorif@gmail.com>
2023-05-15 20:00:03 +0300

    Get courses & by slug

--- src/instructor/instructor.service.ts
+++ src/instructor/instructor.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Course, CourseDocument } from 'src/course/course.model';
 import { User, UserDocument } from 'src/user/user.model';
 import { InstructorApplyDto } from './dto/instructor.dto';
 import { Instructor, InstructorDocument } from './instructor.model';
@@ -10,19 +11,15 @@ export class InstructorService {
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     @InjectModel(Instructor.name) private instructorModel: Model<InstructorDocument>,
+    @InjectModel(Course.name) private courseModel: Model<CourseDocument>,
   ) {}
+
   async applyAsInstructor(dto: InstructorApplyDto) {
     const { email, firstName, lastName, socialMedia } = dto;
     let user: UserDocument;
 
     const existUser = await this.userModel.findOne({ email });
     user = existUser;
-
-    if(user) {
-      await this.userModel.findByIdAndUpdate(user._id, {
-        $set: {job, fullName: `${firstName} ${lastName}`},
-      });
-    }
 
     if (!existUser) {
       const newUser = await this.userModel.create({ ...dto, fullName: `${firstName} ${lastName}` });
@@ -38,5 +35,13 @@ export class InstructorService {
     await this.instructorModel.create(data);
 
     return 'Success';
+  }
+
+  async getAllCourses(author: string) {
+    return await this.courseModel.find({ author });
+  }
+
+  async getDetailedCourse(slug: string) {
+    return await this.courseModel.findOne({ slug });
   }
 }
