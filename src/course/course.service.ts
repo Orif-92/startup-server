9736bdd6ec83d204db0c6b216bc138be9d8318c6
Orif-92 <sarniyozovorif@gmail.com>
2023-05-16 20:21:32 +0300

    Instructor manage

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,20 +1,126 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Instructor, InstructorSchema } from 'src/instructor/instructor.model';
-import { User, UserSchema } from 'src/user/user.model';
-import { CourseController } from './course.controller';
-import { Course, CourseSchema } from './course.model';
-import { CourseService } from './course.service';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Instructor, InstructorDocument } from 'src/instructor/instructor.model';
+import { CourseBodyDto } from './coourse.dto';
+import { Course, CourseDocument } from './course.model';
 
-@Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: User.name, schema: UserSchema },
-      { name: Course.name, schema: CourseSchema },
-      { name: Instructor.name, schema: InstructorSchema },
-    ]),
-  ],
-  controllers: [CourseController],
-  providers: [CourseService],
-})
-export class CourseModule {}
+@Injectable()
+export class CourseService {
+  constructor(
+    @InjectModel(Course.name) private courseModel: Model<CourseDocument>,
+    @InjectModel(Instructor.name) private instructorModel: Model<InstructorDocument>,
+  ) {}
+
+  async createCourse(dto: CourseBodyDto, id: string) {
+    const slugify = (str: string) =>
+      str
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, '')
+        .replace(/[\s_-]+/g, '-')
+        .replace(/^-+|-+$/g, '');
+
+    const slug = slugify(dto.title);
+    const course = await this.courseModel.create({ ...dto, slug: slug, author: id });
+    await this.instructorModel.findOneAndUpdate(
+      { author: id },
+      { $push: { courses: course._id } },
+      { new: true },
+    );
+    return course;
+  }
+
+  async editCourse(dto: CourseBodyDto, courseId: string) {
+    return await this.courseModel.findByIdAndUpdate(courseId, dto, { new: true });
+  }
+
+  async deleteCourse(courseId: string, userId: string) {
+    await this.courseModel.findByIdAndRemove(courseId);
+    await this.instructorModel.findOneAndUpdate(
+      { author: userId },
+      { $pull: { courses: courseId } },
+      { new: true },
+    );
+    return 'Success';
+  }
+
+  async activateCourse(courseId: string) {
+    const course = await this.courseModel.findByIdAndUpdate(
+      courseId,
+      { $set: { isActive: true } },
+      { new: true },
+    );
+
+    return course;
+  }
+
+  async draftCourse(courseId: string) {
+    const course = await this.courseModel.findByIdAndUpdate(
+      courseId,
+      { $set: { isActive: false } },
+      { new: true },
+    );
+
+    return course;
+  }
+
+  async dragCourseSections(courseId: string, sections: string[]) {
+    const course = await this.courseModel
+      .findByIdAndUpdate(courseId, { $set: { sections } }, { new: true })
+      .populate({ path: 'sections', populate: { path: 'lessons' } });
+
+    return course.sections;
+  }
+
+  async getCourses(language: string, limit: string) {
+    const courses = await this.courseModel
+      .find({ language })
+      .populate({ path: 'sections', populate: { path: 'lessons' } })
+      .populate('author')
+      .limit(Number(limit))
+      .sort({ createdAt: -1 })
+      .exec();
+
+    return courses.map(course => this.getSpecificFieldCourse(course));
+  }
+
+  getSpecificFieldCourse(course: CourseDocument) {
+    return {
+      title: course.title,
+      previewImage: course.previewImage,
+      price: course.price,
+      level: course.level,
+      author: {
+        fullName: course.author.fullName,
+        avatar: course.author.avatar,
+      },
+      lessonCount: course.sections.map(c => c.lessons.length).reduce((a, b) => +a + +b, 0),
+      totalHour: this.getTotalHours(course),
+    };
+  }
+
+  getTotalHours(course: CourseDocument) {
+    let totalHours: number = 0;
+
+    for (let i = 0; i < course.sections.length; i++) {
+      const sections = course.sections[i];
+      let sectionHours = 0;
+
+      for (let j = 0; j < sections.lessons.length; j++) {
+        const lesson = sections.lessons[j];
+        const hours = parseInt(String(lesson.hour));
+        const seconds = parseInt(String(lesson.second));
+        const minutes = parseInt(String(lesson.minute));
+        const totalMinutes = hours * 60 + minutes;
+        const totalSeconds = totalMinutes * 60 + seconds;
+        const totalHourLesson = totalSeconds / 3600;
+        sectionHours += totalHourLesson;
+      }
+
+      totalHours += sectionHours;
+    }
+
+    return totalHours.toFixed(1);
+  }
+}
